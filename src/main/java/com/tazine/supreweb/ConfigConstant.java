e9412698b9ef51fba0be486c2fa0b58071428fbc
Frank <bookfrank@foxmail.com>
2017-12-08 16:23:06 +0800

    Supreweb 框架项目启动

---
+++ src/main/java/com/tazine/supreweb/ConfigConstant.java
@@ -0,0 +1,24 @@
+package com.tazine.supreweb;
+
+/**
+ * 提供相关配置项常量
+ *
+ * @author Frank
+ * @since 1.0.0
+ */
+public interface ConfigConstant {
+
+
+    String CONFIG_FILE = "";
+
+    String JDBC_DRIVER = "";
+    String JDBC_URL = "";
+    String JDBC_USERNAME = "";
+    String JDBC_PASSWORD = "";
+
+    String APP_BASE_PACKAGE = "";
+    String APP_JSP_PATH = "";
+    String APP_ASSET_PATH = "";
+
+
+}
