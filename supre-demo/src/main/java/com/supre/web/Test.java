e6514aa244b804174ae9e9c4c11f49f38dacde22
Frank <bookfrank@foxmail.com>
2018-01-23 11:05:30 +0800

    feat (supre-demo) : add the supre-demo module.

---
+++ supre-demo/src/main/java/com/supre/web/Test.java
@@ -0,0 +1,10 @@
+package com.supre.web;
+
+/**
+ * Created by lina on 2018/1/23.
+ *
+ * @author frank
+ * @since 1.0.0
+ */
+public class Test {
+}
---
+++ supre-demo/src/main/java/com/supre/web/controller/IndexController.java
@@ -0,0 +1,16 @@
+package com.supre.web.controller;
+
+import com.supre.framework.annotation.Controller;
+
+/**
+ * Created by lina on 2018/1/23.
+ *
+ * @author frank
+ * @since 1.0.0
+ */
+@Controller
+public class IndexController {
+
+
+
+}
