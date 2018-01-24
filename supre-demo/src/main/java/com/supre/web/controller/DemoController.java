034ab565309f255d72d654696f30b0a538f3d91d
Frank <bookfrank@foxmail.com>
2018-01-24 23:35:25 +0800

    test (supre-demo) : add the demo controller and demo service.

---
+++ supre-demo/src/main/java/com/supre/web/controller/DemoController.java
@@ -0,0 +1,25 @@
+package com.supre.web.controller;
+
+import com.supre.framework.annotation.Autowired;
+import com.supre.framework.annotation.RequestMapping;
+import com.supre.framework.annotation.RequestMethod;
+import com.supre.framework.response.Data;
+import com.supre.web.service.DemoService;
+
+/**
+ * DemoController
+ *
+ * @author frank
+ * @since 1.0.0
+ */
+public class DemoController {
+
+    @Autowired
+    private DemoService demoService;
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public Data hello() {
+        String res = demoService.sayHello();
+        return new Data(res);
+    }
+}
---
+++ supre-demo/src/main/java/com/supre/web/service/DemoService.java
@@ -0,0 +1,16 @@
+package com.supre.web.service;
+
+import com.supre.framework.annotation.Service;
+
+/**
+ * DemoService
+ * @author frank
+ * @since 1.0.0
+ */
+@Service
+public class DemoService {
+
+    public String sayHello(){
+        return "Hello World";
+    }
+}
