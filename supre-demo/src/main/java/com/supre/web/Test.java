251fa9a5f9282eaee08f857d5805ab88d13f88e8
Frank <bookfrank@foxmail.com>
2018-02-06 19:13:16 +0800

    test (supre-demo) : add the test in demo module.

--- supre-demo/src/main/java/com/supre/web/Test.java
+++ supre-demo/src/main/java/com/supre/web/Test.java
@@ -1,10 +1,18 @@
 package com.supre.web;
 
+import com.supre.framework.ioc.ClassContainer;
+
 /**
- * Created by lina on 2018/1/23.
+ * Test
  *
  * @author frank
  * @since 1.0.0
  */
 public class Test {
+
+    public static void main(String[] args) {
+        for (Class<?> c : ClassContainer.getControllerClasses()) {
+            System.out.println(c.getSimpleName());
+        }
+    }
 }
--- supre-demo/src/main/java/com/supre/web/controller/DemoController.java
+++ supre-demo/src/main/java/com/supre/web/controller/DemoController.java
@@ -1,6 +1,7 @@
 package com.supre.web.controller;
 
 import com.supre.framework.annotation.Autowired;
+import com.supre.framework.annotation.Controller;
 import com.supre.framework.annotation.RequestMapping;
 import com.supre.framework.annotation.RequestMethod;
 import com.supre.framework.response.Data;
@@ -12,6 +13,7 @@ import com.supre.web.service.DemoService;
  * @author frank
  * @since 1.0.0
  */
+@Controller
 public class DemoController {
 
     @Autowired
@@ -20,6 +22,7 @@ public class DemoController {
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public Data hello() {
         String res = demoService.sayHello();
+        System.out.println("Hello World");
         return new Data(res);
     }
 }
